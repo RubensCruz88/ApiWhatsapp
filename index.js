496a26bdfd187667f6180272028f608611ec04be
Rubens Cruz <rubenscruz88@gmail.com>
2022-03-20 14:29:56 -0300

    Alterado forma de exibição do QrCode para arquivo png

--- index.js
+++ index.js
@@ -7,18 +7,39 @@ const config = require('config');
 venom
 	.create(
 		config.get("whatsApp.nomeSessao"),
-		undefined,
+		(base64Qr, asciiQR, attempts, urlCode) => {
+			console.log(asciiQR); // Optional to log the QR in the terminal
+			var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+				response = {};
+		
+			if (matches.length !== 3) {
+				return new Error('Invalid input string');
+			}
+			response.type = matches[1];
+			response.data = new Buffer.from(matches[2], 'base64');
+		
+			var imageBuffer = response;
+			require('fs').writeFile(
+				'./tokens/' + config.get("whatsApp.nomeSessao") + '/' + config.get("api.imagemQrCode") + '.png',
+				imageBuffer['data'],
+				'binary',
+				function (err) {
+					if (err != null) {
+						console.log(err);
+					}
+				}
+			);
+		},
 		(statusSession, session) => {
 			// return: isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken
 			console.log('Status Session: ', statusSession);
 			// create session wss return "serverClose" case server for close
 			console.log('Session name: ', session);
 		},
-		undefined,
 		{
-			headless: false,
-			useChrome: true,
-			logQR: false
+			logQR: false,
+			disableSpins: true,
+			disableWelcome: true
 		}
 	)
 	.then((client) => start(client))
@@ -39,19 +60,6 @@ function start(client) {
 	app.listen(config.get('api.porta'), () => console.log('A Api está funcionando'));
 
 	app.use('/api/envio',roteador);
-
-	app.post('/mensagem',(req, res) => {
-		const body = req.body;
-	
-		if (body.mensagem == undefined){
-			res.status(400).send("mensagem não informada");
-			return
-		}else if (body.telefone == undefined){
-			res.status(400).send("telefone não informado");
-			return
-		}
-	
-	});
 
 	// In case of being logged out of whatsapp web
 	// Force it to keep the current session
